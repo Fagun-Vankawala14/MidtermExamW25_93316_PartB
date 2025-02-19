efec7779fa534294d03cb91542caa13a58a2ed68
HP <HP@10.48.48.242>
2025-02-19 10:44:23 -0500

    Availability change

--- src/midtermexamw25_93316_partb/AvailabilityChecking.java
+++ src/midtermexamw25_93316_partb/AvailabilityChecking.java
@@ -1,13 +1,21 @@
 package midtermexamw25_93316_partb;
 
+import java.awt.print.Book;
+
 public class AvailabilityChecking {
 
     private static boolean[] bookAvailability = {true, true, true, true};
 
     public boolean isBookAvailable(int index) {
-        //Complete this method to check book availability
-        return true;
+        Iterable<Book> books = null;
+        for (Book book : books) {
+            boolean bookId = false;
+        if (book.getId() == bookId) {
+            return true;
+        }
     }
+    return false;
+}
 
     public void borrowBook(int index) {
         if (isBookAvailable(index)) {
