4d1eb9fa18c5cbf3ef2b7ca86b9c442aace62c17
squid <sidhu769@sheridancollege.ca>
2025-02-19 10:30:31 -0500

    Final Commit

---
+++ src/midtermexamw25_93316_partb/Books.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexamw25_93316_partb;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author anmol
+ */
+
+//This class follows Encapsulation by making book data private and providing controlled access..
+public class Books {
+    
+    private static String[] books = {"The Great Gatsby", "1984", "To Kill a Mockingbird", "The Catcher in the Rye"};
+    
+    private AvailabilityChecking availabilityChecker = new AvailabilityChecking();
+    
+    public void displayCatalog() {
+        
+        System.out.println("Welcome to the library! Here is a list of available books:");
+        
+        for(String book: books ){
+            System.out.println(book);
+        }
+    }
+    
+    public void selectBook(Scanner sc) {
+        System.out.print("\nEnter the Book number which you want to borrow: ");
+        int choice = sc.nextInt() - 1;
+
+        if (choice < 0 || choice >= books.length) {
+            System.out.println("Invalid selection. Please try again.");
+            return;
+        }
+
+        if (availabilityChecker.isBookAvailable(choice)) {
+            System.out.println("You selected: " + books[choice] + ". Enjoy your reading!");
+            availabilityChecker.borrowBook(choice); // Updates availability in AvailabilityChecking
+        } else {
+            System.out.println("Sorry, " + books[choice] + " is currently unavailable. Please select another book.");
+        }
+    }
+}
