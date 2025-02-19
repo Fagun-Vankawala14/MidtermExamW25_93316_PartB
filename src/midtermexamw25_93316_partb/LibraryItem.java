75bb1cd0edd9044a5eedc23b2356b8db8bfe816e
Bharat023 <bharat0@sheridancollege.ca>
2025-02-19 10:18:15 -0500

    Initial commit

--- src/midtermexamw25_93316_partb/LibraryItem.java
+++ src/midtermexamw25_93316_partb/LibraryItem.java
@@ -3,7 +3,7 @@ package midtermexamw25_93316_partb;
 import java.util.Scanner;
 
 public class LibraryItem {
-
+//add
     private static String[] books = {"The Great Gatsby", "1984", "To Kill a Mockingbird", "The Catcher in the Rye"};
     private AvailabilityChecking availabilityChecker = new AvailabilityChecking();
 
