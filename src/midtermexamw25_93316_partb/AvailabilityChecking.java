0654eb22d4112537ec397fc02695440db14532da
NISHANT NISHANT <nishant8@shernet.sheridancollege.ca>
2025-02-19 10:21:58 -0500

    book availabile

--- src/midtermexamw25_93316_partb/AvailabilityChecking.java
+++ src/midtermexamw25_93316_partb/AvailabilityChecking.java
@@ -5,20 +5,28 @@ public class AvailabilityChecking {
     private static boolean[] bookAvailability = {true, true, true, true};
 
     public boolean isBookAvailable(int index) {
-        //Complete this method to check book availability
-        //Complete this method to check book availability
-        return true;
+        // Check if the index is within the valid range
+        if (index >= 0 && index < bookAvailability.length) {
+            return bookAvailability[index]; 
+        }
+        return false; 
     }
 
     public void borrowBook(int index) {
-        if (isBookAvailable(index)) {
+        if (isBookAvailable(index)) { // Check availability before borrowing
             bookAvailability[index] = false;
+            System.out.println("Book at index " + index + " has been borrowed.");
+        } else {
+            System.out.println("Book at index " + index + " is not available.");
         }
     }
 
     public void returnBook(int index) {
         if (index >= 0 && index < bookAvailability.length) {
             bookAvailability[index] = true;
+            System.out.println("Book at index " + index + " has been returned.");
+        } else {
+            System.out.println("Invalid book index.");
         }
     }
 }
