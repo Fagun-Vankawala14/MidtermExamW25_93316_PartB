a19942a3075bea7229322633b3d90ded6fdd0763
Amanpreet kaur <kau17785@sheridancollege.ca>
2025-02-19 09:58:26 -0500

    completed the displaycatalog() method so that it can display the list of books

--- src/midtermexamw25_93316_partb/LibraryItem.java
+++ src/midtermexamw25_93316_partb/LibraryItem.java
@@ -10,8 +10,10 @@ public class LibraryItem {
     public void displayCatalog() {
         System.out.println("Welcome to the library! Here is a list of available books:");
         //Comaplete this method to display list of books
-    }
-
+        for (int i=0; i< books.length; i++){
+            System.out.println((i+1)+"."+books[i]);
+        }} 
+    
     public void selectBook(Scanner sc) {
         System.out.print("\nEnter the Book number which you want to borrow: ");
         int choice = sc.nextInt() - 1;
