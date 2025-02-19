905f327a055cb182a0eb46d137e2c925e1bdc653
squid <sidhu769@sheridancollege.ca>
2025-02-19 10:27:15 -0500

    Final Commit

--- src/midtermexamw25_93316_partb/LibraryItem.java
+++ src/midtermexamw25_93316_partb/LibraryItem.java
@@ -4,44 +4,20 @@ import java.util.Scanner;
 
 public class LibraryItem {
 
-    private static String[] books = {"The Great Gatsby", "1984", "To Kill a Mockingbird", "The Catcher in the Rye"};
-    private AvailabilityChecking availabilityChecker = new AvailabilityChecking();
-
-    public void displayCatalog() {
-        
-        System.out.println("Welcome to the library! Here is a list of available books:");
-        
-        for(String book: books ){
-            System.out.println(book);
-        }
-        //Comaplete this method to display list of books
-    }
-
-    public void selectBook(Scanner sc) {
-        System.out.print("\nEnter the Book number which you want to borrow: ");
-        int choice = sc.nextInt() - 1;
-
-        if (choice < 0 || choice >= books.length) {
-            System.out.println("Invalid selection. Please try again.");
-            return;
-        }
-
-        if (availabilityChecker.isBookAvailable(choice)) {
-            System.out.println("You selected: " + books[choice] + ". Enjoy your reading!");
-            availabilityChecker.borrowBook(choice); // Updates availability in AvailabilityChecking
-        } else {
-            System.out.println("Sorry, " + books[choice] + " is currently unavailable. Please select another book.");
-        }
-    }
+//    We used Single Responsible principle from SOLID here 
+//    and removed the unnecessary methods which were not needed in the main method 
+//    and kept the main method clean and empty thus we also dont need to have instance of main method now.
 
     public static void main(String[] args) {
+        Books books = new Books();
+        
         Scanner sc = new Scanner(System.in);
-        LibraryItem libraryItem = new LibraryItem(); // Create an instance of LibraryItem
+            
         boolean continueBorrowing = true;
 
         while (continueBorrowing) {
-            libraryItem.displayCatalog();
-            libraryItem.selectBook(sc); 
+            books.displayCatalog();
+            books.selectBook(sc);
 
             System.out.print("\nWould you like to borrow another book? (yes/no): ");
             String response = sc.next().trim().toLowerCase();
