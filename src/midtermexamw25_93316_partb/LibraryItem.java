0e722f573043352eaefb51a7398592308e3caf9c
Reinard Villar <villarr@shernet.sheridancollege.ca>
2025-02-19 09:42:27 -0500

    displayCatalog

--- src/midtermexamw25_93316_partb/LibraryItem.java
+++ src/midtermexamw25_93316_partb/LibraryItem.java
@@ -5,45 +5,74 @@ import java.util.Scanner;
 public class LibraryItem {
 
     private static String[] books = {"The Great Gatsby", "1984", "To Kill a Mockingbird", "The Catcher in the Rye"};
+
     private AvailabilityChecking availabilityChecker = new AvailabilityChecking();
 
     public void displayCatalog() {
+
         System.out.println("Welcome to the library! Here is a list of available books:");
-        //Comaplete this method to display list of books
+
+        //Complete this method to display list of books
+        for (int i = 0; i < books.length; i++) {
+            System.out.println((i + 1) + ". " + books[i]);
+        }
+
     }
 
     public void selectBook(Scanner sc) {
+
         System.out.print("\nEnter the Book number which you want to borrow: ");
+
         int choice = sc.nextInt() - 1;
 
         if (choice < 0 || choice >= books.length) {
+
             System.out.println("Invalid selection. Please try again.");
+
             return;
+
         }
 
         if (availabilityChecker.isBookAvailable(choice)) {
+
             System.out.println("You selected: " + books[choice] + ". Enjoy your reading!");
-            availabilityChecker.borrowBook(choice); // Updates availability in AvailabilityChecking
+
+            availabilityChecker.borrowBook(choice);
+
         } else {
+
             System.out.println("Sorry, " + books[choice] + " is currently unavailable. Please select another book.");
+
         }
+
     }
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        LibraryItem libraryItem = new LibraryItem(); // Create an instance of LibraryItem
+
+        LibraryItem libraryItem = new LibraryItem();
+        
         boolean continueBorrowing = true;
 
         while (continueBorrowing) {
+
             libraryItem.displayCatalog();
-            libraryItem.selectBook(sc); 
+
+            libraryItem.selectBook(sc);
 
             System.out.print("\nWould you like to borrow another book? (yes/no): ");
+
             String response = sc.next().trim().toLowerCase();
+
             continueBorrowing = response.equals("yes");
+
         }
 
         System.out.println("Thank you for using the library!");
+
         sc.close();
+
     }
+
 }
